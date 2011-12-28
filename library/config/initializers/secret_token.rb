f472cf2170c609ddc94923f78957b7658e81bbc9
NotarySojac <wastyx@gmail.com>
2011-12-28 14:30:16 -0600

    initial commit

---
+++ library/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Library::Application.config.secret_token = 'c2d86edec2290dbabd3d4c4fa2365fb9c5ec01b9650ca630c157c94ec1daa58f3a8d1e4cd748063672d471b6a7a7f7ed764db43a4e6c5475fb0cf3747d2c5e5f'
---
+++ railsClient/railsClient/Program.cs
@@ -0,0 +1,114 @@
+﻿// compiles with 
+// $  gmcs railsClient.cs
+
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.IO;
+using System.Text;
+
+
+public class railsClient
+{
+    static public void Main()
+    {
+        Console.WriteLine("We're going to send an XML file to our rails database!");
+
+
+        Console.WriteLine("Please input the path to your rails app and it's controller (or leave blank if you coded a default):");
+
+        string path = Console.ReadLine();
+        string response = "";
+
+        if (path == "")
+            response = sendXmlData("http://192.168.0.11:3000/books");
+        else
+            response = sendXmlData(path);
+
+
+        Console.WriteLine("\n\n Server Response: \n");
+        Console.WriteLine(response);
+        Console.WriteLine("\n");
+
+
+        Console.WriteLine("We're done!");
+        Console.Read();
+    }
+
+    // return true if data is successfully stored (not yet working!)
+    static private string sendXmlData(string pathToAction)
+    {
+        string response = "";
+
+        HttpWebRequest req = null;
+        HttpWebResponse rsp = null;
+        try
+        {
+            string uri = pathToAction;
+
+            string xmlData = getXmlDataString();
+
+            req = setupWebRequest(uri);
+
+            // Wrap the request stream with a text-based writer
+            StreamWriter writer = new StreamWriter(req.GetRequestStream());
+            // Write the XML text into the stream
+            writer.WriteLine(xmlData);
+            writer.Close();
+
+            // Send the data to the webserver
+            rsp = (HttpWebResponse)req.GetResponse();
+
+            Encoding enc = System.Text.Encoding.GetEncoding(1252);
+            StreamReader loResponseStream = new StreamReader(rsp.GetResponseStream(), enc);
+
+            response = loResponseStream.ReadToEnd();
+
+            string statusCode = rsp.StatusCode.ToString();
+            response = "Status Code: " +statusCode + "\n\n ResponseStream:\n" + response;
+        }
+        catch (WebException webEx)
+        {
+            // 
+        }
+        catch (Exception ex)
+        {
+
+        }
+        finally
+        {
+            if (req != null) req.GetRequestStream().Close();
+            if (rsp != null) rsp.GetResponseStream().Close();
+        }
+
+        return response;
+    }
+
+    static public HttpWebRequest setupWebRequest(string uri)
+    {
+        HttpWebRequest req = (HttpWebRequest)WebRequest.Create(uri);
+        req.Method = "POST";        // Post method
+        req.ContentType = "text/xml";     // content type
+        req.Accept = "text/xml";
+
+        return req;
+    }
+
+
+    static public string getXmlDataString()
+    {
+        string xmlData = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+        xmlData += "<book>";
+        xmlData += "<author>Ex Emel</author>";
+        xmlData += "<created-at type=\"datetime\">2008-05-26T05:58:38Z</created-at>";
+        xmlData += "<id type=\"integer\">2</id>";
+        xmlData += "<isbn>1234567890</isbn>";
+        xmlData += "<price type=\"decimal\">34.99</price>";
+        xmlData += "<title>Posted via XML 2</title>";
+        xmlData += " <updated-at type=\"datetime\">2008-05-26T05:58:38Z</updated-at>";
+        xmlData += "</book>";
+
+        return xmlData;
+    }
+}
+
